f9521a57234bd4bbc0b54e94cc298fbaa969f826
Kezzoul Massili <massy.kezzoul@gmail.com>
2020-11-04 11:27:58 +0100

    sql marche

--- premier_rendu/scriptCreation.sql
+++ premier_rendu/scriptCreation.sql
@@ -1,15 +1,16 @@
 DROP TABLE ventes_billets;
-DROP TABLE dimension_aereport;
+DROP TABLE stock_avions;
+DROP TABLE dimension_aeroport;
+DROP TABLE dimension_client;
 DROP TABLE dimension_vol;
 DROP TABLE dimension_avion;
 DROP TABLE dimension_heureJour;
 DROP TABLE dimension_date;
 
 
-
 CREATE TABLE dimension_date
 (
-    id NUMBER(5) PRIMARY KEY AUTO_INCREMENT,
+    id NUMBER(5) PRIMARY KEY,
     format_date VARCHAR2(20),
     jour_de_semaine NUMBER(1) CHECK(jour_de_semaine IN (1,2,3,4,5,6,7)),
     num_jour NUMBER(2) CHECK(num_jour BETWEEN 1 AND 31),
@@ -28,7 +29,7 @@ CREATE TABLE dimension_date
 );
 CREATE TABLE dimension_heureJour
 (
-    id NUMBER(5) PRIMARY KEY AUTO_INCREMENT,
+    id NUMBER(5) PRIMARY KEY,
     heure_jour NUMBER(2) NOT NULL,
     minute_heure NUMBER(2) NOT NULL, 
     fuseau_horaire VARCHAR2(5) NOT NULL, 
@@ -37,7 +38,7 @@ CREATE TABLE dimension_heureJour
 );
 CREATE TABLE dimension_avion 
 (
-    id NUMBER(5) PRIMARY KEY AUTO_INCREMENT,
+    id NUMBER(5) PRIMARY KEY,
     nom_avion VARCHAR2(10) NOT NULL,
     capacite NUMBER(5),
     conso_par_km NUMBER(5),
@@ -54,7 +55,7 @@ CREATE TABLE dimension_avion
 
 CREATE TABLE dimension_vol
 (
-    id NUMBER(5) PRIMARY KEY AUTO_INCREMENT,
+    id NUMBER(5) PRIMARY KEY,
     num_vol NUMBER(10) NOT NULL,
     num_avion NUMBER(5), 
     FOREIGN KEY(num_avion) REFERENCES dimension_avion(id),
@@ -72,27 +73,27 @@ CREATE TABLE dimension_vol
 
 CREATE TABLE dimension_client
 (
-    id NUMBER(5) PRIMARY KEY AUTO_INCREMENT,
+    id NUMBER(5) PRIMARY KEY,
     nom VARCHAR2(20),
     prenom VARCHAR2(20),
     date_naissance date,
     age NUMBER(3),
     tranche_age VARCHAR2(20) CHECK (tranche_age IN ('0-18','19-26','26-40','41+')),
     nationalite VARCHAR2(20),
-    fidelite VARCHAR2(3) CHECK (fidelite IN ('oui','non')
+    fidelite VARCHAR2(3) CHECK (fidelite IN ('oui','non'))
 );
 
 
-CREATE TABLE dimension_aereport
+CREATE TABLE dimension_aeroport
 (
-    id NUMBER(5) PRIMARY KEY AUTO_INCREMENT,
+    id NUMBER(5) PRIMARY KEY,
     ville VARCHAR2(20) NOT NULL,    
     pays VARCHAR2(20) NOT NULL,
     num_rue NUMBER(3) NOT NULL,
     nom_rue VARCHAR2(20) NOT NULL,
     code_postal NUMBER(5) NOT NULL,
-    nom_aereport VARCHAR2(20) NOT NULL,
-    type_aereport VARCHAR2(20),
+    nom_aeroport VARCHAR2(20) NOT NULL,
+    type_aeroport VARCHAR2(20),
     transport_commerciale VARCHAR2(3) CHECK (transport_commerciale IN ('oui','non')),
     transport_feret VARCHAR2(3) CHECK (transport_feret IN ('oui','non')),
     categorie VARCHAR2(20),
@@ -111,10 +112,10 @@ CREATE TABLE ventes_billets
     FOREIGN KEY(heure_depart) REFERENCES dimension_heureJour(id),
     heure_arrivee NUMBER(5),
     FOREIGN KEY(heure_arrivee) REFERENCES dimension_heureJour(id),
-    aereport_depart NUMBER(5),
-    FOREIGN KEY(aereport_depart) REFERENCES dimension_aereport(id),
-    aereport_arrivee NUMBER(5),
-    FOREIGN KEY(aereport_arrivee) REFERENCES dimension_aereport(id),
+    aeroport_depart NUMBER(5),
+    FOREIGN KEY(aeroport_depart) REFERENCES dimension_aeroport(id),
+    aeroport_arrivee NUMBER(5),
+    FOREIGN KEY(aeroport_arrivee) REFERENCES dimension_aereport(id),
     num_vol NUMBER(5),
     FOREIGN KEY(num_vol) REFERENCES dimension_vol(id),
     num_client NUMBER(5),
@@ -123,4 +124,18 @@ CREATE TABLE ventes_billets
     type_billet VARCHAR2(20),
     poids_autorise NUMBER(5),
     poids_enregistre NUMBER(5)
+);
+
+CREATE TABLE stock_avions
+(
+    num_avion NUMERIC(5),
+    FOREIGN KEY(num_avion) REFERENCES dimension_avion(id),
+    date_jour NUMERIC(5),
+    FOREIGN KEY(date_jour) REFERENCES dimension_date(id),
+    aeroport NUMERIC(5),
+    FOREIGN KEY(aeroport) REFERENCES dimension_aeroport(id),
+    nombre_vol NUMBER(5),
+    nombre_km NUMBER(5),
+    carburant_consomme NUMBER(5),
+    recette NUMBER(5)
 );
